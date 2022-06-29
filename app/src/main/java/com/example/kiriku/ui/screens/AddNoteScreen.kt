a7beeca41f9e340d28c2a625e8022630c281892e
karlmax okoth <karlmaxokoth@karlmaxs-MacBook-Pro.local>
2022-06-29 14:04:04 +0300

    added notes screen

---
+++ app/src/main/java/com/example/kiriku/ui/screens/AddNoteScreen.kt
@@ -0,0 +1,31 @@
+package com.example.kiriku.ui.screens
+
+import androidx.compose.material.Button
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import com.example.kiriku.database.local.Notes
+import com.example.kiriku.ui.viewmodel.NotesViewModel
+
+@Composable
+fun addNotesScreen(viewModel: NotesViewModel){
+    val title  = remember{ mutableStateOf("")}
+    val body  = remember{ mutableStateOf("")}
+    TextField(value = title.value, onValueChange = {
+        title.value = it
+    })
+
+    TextField(value = body.value, onValueChange = {
+        body.value = it
+    })
+
+    Button(onClick = {
+        viewModel.addNotes(notes = Notes(
+            title = title.value,
+            body = body.value
+        ))
+    }) {
+
+    }
+}
